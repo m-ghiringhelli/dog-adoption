99ed98a6d25e25bb3a031bddcf5c564f8f363963
m-ghiringhelli <mghiring@gmail.com>
2022-03-28 14:35:36 -0700

    added browser router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import Home from './views/Home/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './views/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
