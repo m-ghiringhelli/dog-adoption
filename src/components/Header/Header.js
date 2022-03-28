befcdb68c98b0f772ab10b8ac055ffb7743975ca
m-ghiringhelli <mghiring@gmail.com>
2022-03-28 14:39:41 -0700

    created basic header for nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header className='header'>
+      <ul>
+        <li>
+          <NavLink exact to='/'>
+            Home
+          </NavLink>
+        </li>
+      </ul>
+    </header>
   );
 }
