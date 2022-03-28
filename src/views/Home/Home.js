5843309b336cc75b75210a0c4263a518cf0e81c8
m-ghiringhelli <mghiring@gmail.com>
2022-03-28 14:18:12 -0700

    home loads data

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchDogs } from '../../services/dogs';
 
 export default function Home() {
-  const dogs = fetchDogs();
-  console.log(dogs);
+  const [dogs, setDogs] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const request = await fetchDogs();
+        setDogs(request);
+      } catch (e) {
+        setError('trouble loading dogs');
+      }
+    };
+    fetchData();
+  }, []);
+  
   return (
     <div>Home</div>
   );
