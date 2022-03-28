44a944e4d55ad83a7ff6a17385ae2acaf8ac5000
m-ghiringhelli <mghiring@gmail.com>
2022-03-28 15:37:01 -0700

    added loading to fix race issue

--- src/views/DogDetail/DogDetail.js
+++ src/views/DogDetail/DogDetail.js
@@ -4,12 +4,17 @@ import { fetchDogById } from '../../services/dogs';
 
 export default function DogDetail() {
   const id = useParams().id;
+  const [loading, setLoading] = useState(true);
   const [dog, setDog] = useState();
   
   useEffect(() => {
     fetchDogById(id).then((data) => setDog(data));
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
   }, [id]);
 
+  if (loading) return <div>loading...</div>;
 
   return (
     <div>
